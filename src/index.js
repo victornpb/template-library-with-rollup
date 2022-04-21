81dd7e52124f472d9a3ec38f00be81047a70d33f
victornpb <3372598+victornpb@users.noreply.github.com>
2022-04-21 12:32:36 -0300

    eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import sum from './utils/sum';
 
 export default class FooBar {
-  str = "Hello World";
+  str = 'Hello World';
   #p = 1;
   constructor() {
-    console.log("FooBar" + this.str + this.#p);
+    console.log('FooBar' + this.str + this.#p);
   }
   sum(a, b) {
     return sum(a, b);
